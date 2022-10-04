f5caa957c79d96e02ca8388c36b2b7d87210680e
DavGuett <soulsec@protonmail.com>
2022-10-04 20:51:38 -0300

    Add project files.

---
+++ ListarPessoas/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pessoas
+{
+    internal class Pessoa
+    {
+        public int idade;
+        public string nome;
+        public char sexo;
+
+        public Pessoa(int idade, string nome, char sexo)
+        {
+            this.idade = idade;
+            this.nome = nome;
+            this.sexo = sexo;
+        }
+
+        public string listarInformacoes()
+        {
+            return $"{nome} tem {idade} anos de idade e é do sexo {sexo}";
+        }
+    }
+}
---
+++ ListarPessoas/Program.cs
@@ -0,0 +1,23 @@
+﻿using Pessoas;
+
+Console.WriteLine("Quantas pessoas serão adicionadas?");
+int numeroDePessoas = int.Parse(Console.ReadLine());  
+
+List<Pessoa> listaPessoas = new List<Pessoa>();
+
+for (int i = 0; i < numeroDePessoas; i++)
+{
+    Console.WriteLine("Digite o nome da pessoa N°" + i + " ");
+    string nome = Console.ReadLine();
+    Console.WriteLine("Digite a idade da pessoa N°" + i + " ");
+    int idade = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o sexo da pessoa N°" + i + " ");
+    char sexo = char.Parse(Console.ReadLine());
+    listaPessoas.Add(new Pessoa(idade, nome, sexo));
+    Console.WriteLine();
+}
+Console.WriteLine("---------------------------");
+foreach (Pessoa pessoa in listaPessoas)
+{
+    Console.WriteLine(pessoa.listarInformacoes());
+}
